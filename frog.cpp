076338c3e7991dfc180f5bfa6c84b54406b1d67d
Ryosuke <yoshimoto2107@gmail.com>
2023-12-17 20:04:43 +0900

    :sparkles:

---
+++ frog.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const long long INF = 1LL << 60;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> h(N);
+    for (size_t i = 0; i < N; i++)
+    {
+        cin >> h[i];
+    }
+
+    vector<long long> dp(N, INF);
+
+    dp[0] = 0;
+
+    for (size_t i = 1; i < N; i++)
+    {
+        if (i == 1) {
+            dp[i] = abs(h[i] - h[i - 1]);
+        }
+        else {
+            dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]));
+        }
+    }
+
+    cout << dp[N - 1] << endl;
+}
