89935da322719012ce841f2fb48a2176dfcb22c4
Ryosuke <yoshimoto2107@gmail.com>
2024-05-06 11:23:22 +0900

    :sparkles:

--- chapter4/iterator.cpp
+++ chapter4/iterator.cpp
@@ -7,25 +7,9 @@ using namespace std;
 int main()
 {
     set<int> s = {2, 5, 6, 8};
-    int x = 7;
-    auto it = s.lower_bound(x);
-    if (it == s.begin())
+    cout << s.size() << "\n"; // 4
+    for (auto x : s)
     {
-        cout << *it << "\n";
-    }
-    else if (it == s.end())
-    {
-        it--;
-        cout << *it << "\n";
-    }
-    else
-    {
-        int a = *it;
-        it--;
-        int b = *it;
-        if (x - b < a - x)
-            cout << b << "\n";
-        else
-            cout << a << "\n";
+        cout << x << "\n";
     }
 }
